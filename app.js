407912724f28f997cd0b93a9a3522855ddc2571a
Adam Chamberlin <adam@shibbyonline.co.uk>
2015-01-29 12:42:59 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// app.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+
+module.exports = app;
